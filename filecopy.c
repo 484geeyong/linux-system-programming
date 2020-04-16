e81be3c54d41a236ed181119dec6b3f88131eb2e
484geeyong <wlwyd30@gmail.com>
2020-04-16 11:50:29 +0900

    hw1

---
+++ filecopy.c
@@ -0,0 +1,42 @@
+# include <stdio.h>
+#define BUFFER_SIZE 1024
+int main(int argc, char *argv[]){
+    char ubuf[BUFFER_SIZE], fbuf[BUFFER_SIZE];
+    int n;
+    FILE *fpin, *fpout;
+    if(argc != 3) {
+        perror(argv[0]);
+        return 1;
+        
+    }
+    if((fpin = fopen(argv[1], "r")) == NULL) {
+        perror(argv[1]);
+        return 2;
+        
+    }
+    if((fpout = fopen(argv[2], "w")) == NULL) {
+        perror(argv[2]);
+        return 3;
+        
+    }
+    if (setvbuf(fpin,ubuf,_IOFBF,BUFFER_SIZE) != 0){
+        perror("setvbuf(fpin)");
+        return 4;
+        
+    }
+    if (setvbuf (fpout, ubuf, _IOFBF, BUFFER_SIZE) != 0) {
+        perror("setvbuf(fpout)");
+        return 5;
+        
+    }
+        n=fread(fbuf, sizeof(char),BUFFER_SIZE,fpin);
+        while (n>0){
+            fwrite (fbuf,sizeof(char),n,fpout);
+            n=fread(fbuf, sizeof(char),BUFFER_SIZE,fpin);
+            
+        
+    }
+    fclose(fpin);
+    fclose(fpout);
+    return 0;
+}
